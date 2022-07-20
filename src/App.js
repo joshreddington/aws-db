d6427a5f1d154f10ddce2d746e462f90d9b09d8f
joshreddington <joshred8@gmail.com>
2022-07-20 19:22:42 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from V2</h1>
+        <p>I wonder why this doesn't work?</p>
       </header>
     </div>
   );
